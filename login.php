5ec77e6e0a1e08e44916195e2a7fa08a1bca71ba
Frédéric <frederichermans@hotmail.com>
2020-03-21 21:31:13 +0100

    Bootstrap css file

--- login.php
+++ login.php
@@ -26,6 +26,7 @@ include_once (__DIR__ . "/classes/Validate.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Amigos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
