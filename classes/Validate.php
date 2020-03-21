f785b8408d7cfb9d6709fba499ab536129afd5fe
Frédéric <frederichermans@hotmail.com>
2020-03-21 17:59:49 +0100

    Functie validatePassword toegevoegd
    Checkt of het password leeg is.

--- classes/Validate.php
+++ classes/Validate.php
@@ -9,18 +9,22 @@ class Validate {
   public function __construct($post_data){
     $this->data = $post_data;
   }
-  // kijkt na of al de $fields in het formulier zitten
+
   public function validateForm(){
-     foreach(self::$fields as $field){
+
+    foreach(self::$fields as $field){
       if(!array_key_exists($field, $this->data)){
         trigger_error("'$field' is not present in the data");
         return;
       }
     }
+
     $this->validateEmail();
+    $this->validatePassword();
     return $this->errors;
+
   }
-  // Is email leeg of geen emailadress
+  // Checht of de email leeg is of het een geldig emailadress is
   private function validateEmail(){
 
     $val = trim($this->data['email']);
@@ -31,6 +35,16 @@ class Validate {
       if(!filter_var($val, FILTER_VALIDATE_EMAIL)){
         $this->addError('email', 'email must be a valid email address');
       }
+    }
+
+  }
+  // Checkt of het password leeg is
+  private function validatePassword(){
+
+    $val = trim($this->data['password']);
+
+    if(empty($val)){
+      $this->addError('password', 'password cannot be empty');
     }
 
   }
