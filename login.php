745661abb12687008ebcaa11b6725c2c070d8cd7
Frédéric <frederichermans@hotmail.com>
2020-03-21 14:17:24 +0100

    Login formulier aangemaakt in HTML

--- login.php
+++ login.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+<form action="" method="post" >
+	<h2>Login Amigos</h2>	
+	 <div class="container">
+			<div class="container1">
+                <label for="email">Email</label>
+				<input type="text" id="Email" name="email">
+			</div>
+			<div class="container2">
+				<label for="Password">Password</label>
+				<input type="password" id="Password" name="password">
+			</div>
+			<div class="container3">
+				<input type="submit" value="Sign in" class="btn">	
+			</div>
+		<p>geen account? </p><a href="register.php" class="link">registreer hier!</a>
+		</div>
+	</form>
 </body>
 </html>
