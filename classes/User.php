fec6fd89d0b068d4bdc28a4ab8405826ca90806f
Frédéric <frederichermans@hotmail.com>
2020-03-21 18:06:09 +0100

    Update User.php

--- classes/User.php
+++ classes/User.php
@@ -10,7 +10,7 @@ include_once (__DIR__ . "/Db.php");
     $statement = $conn->prepare('select * from users where email = :email');
     $statement->bindParam(':email', $email);
     $statement->execute();
-    
+
     $result = $statement->fetch(PDO::FETCH_ASSOC);
     if (password_verify($password, $result['password'])) {
         return true;
