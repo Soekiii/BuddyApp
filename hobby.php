4e698aa02763f5f985c335063b6b321a41356880
davyceuppens <davy.ceuppens@hotmail.be>
2020-03-27 19:23:05 +0100

    feature 4 update

--- hobby.php
+++ hobby.php
@@ -13,7 +13,15 @@ $userID = implode(" ", $userArray);
 if(!empty($_POST)){
     try{
         $hobby = new Hobby();
-        $hobby->hobbyInvullen($_POST);
+        $hobby->setLocatie(htmlspecialchars($_POST['locatie']));
+        $hobby->setHobby(htmlspecialchars($_POST['hobby']));
+        $hobby->setFilm(htmlspecialchars($_POST['film']));
+        $hobby->setGame(htmlspecialchars($_POST['game']));
+        $hobby->setMuziek(htmlspecialchars($_POST['muziek']));
+        $hobby->setUserID(htmlspecialchars($userID));
+        $statement=$hobby->hobbyInvullen($locatie,$hobby,$film,$game,$muziek,$userID);
+        //$hobby->hobbyInvullen();
+        //$hobby->hobbyInvullen($_POST);
         header('Location: index.php');
     }
     catch(throwable $e){
@@ -40,32 +48,31 @@ else{
     </style>
 <body> 
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" > 
-    <h1>Wat zijn je hobbies?</h1>
-    <p>Vul deze 5 velden in met je interesses / hobbies.</p>
+    <h1>Hey! Vul deze velden in om nog makkelijker een match te vinden!</h1>
     <!-- als er een error bestaat -->
     <?php if(isset($error)): ?>
         <div class="error"><p><?php echo $error?></p></div>
     <?php endif; ?>
 
     <div class="">
-        <label for="hobby">Hobby 1</label>
+        <label for="locatie">Vanwaar ben je?</label>
+        <input type="text" id="locatie" name="locatie">
+    </div>
+    <div class="">
+        <label for="hobby">Wat is je hobby?</label>
         <input type="text" id="hobby" name="hobby">
     </div>
     <div class="">
-        <label for="hobby">Hobby 2</label>
-        <input type="text" id="hobby" name="hobby">
+        <label for="game">Welk spel speel je graag?</label>
+        <input type="text" id="game" name="game">
     </div>
     <div class="">
-        <label for="hobby">Hobby 3</label>
-        <input type="text" id="hobby" name="hobby">
+        <label for="film">Naar wat voor gerne filmen kijk je graag?</label>
+        <input type="text" id="film" name="film">
     </div>
     <div class="">
-        <label for="hobby">Hobby 4</label>
-        <input type="text" id="hobby" name="hobby">
-    </div>
-    <div class="">
-        <label for="hobby">Hobby 5</label>
-        <input type="text" id="hobby" name="hobby">
+        <label for="muziek">Naar wat voor gerne muziek luister je graag?</label>
+        <input type="text" id="muziek" name="muziek">
     </div>
         
     <div><input type="submit" value="Add!" class="btn"></div>
