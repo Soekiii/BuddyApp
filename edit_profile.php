fedebf1141779d01f8d98c168c2a86708bbee6f3
Soekiii <61419115+Soekiii@users.noreply.github.com>
2020-03-30 14:01:17 +0200

    HTML geschreven voor checkbox 'zoek buddy'/'ben buddy' on edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -11,5 +11,18 @@
     <div class="bio"><a href="edit_profile\bio.php">Edit bio</a></div>
     <div class="email"><a href="edit_profile\email.php">Change email</a></div>
     <div class="password"><a href="edit_profile\password.php">Change password</a></div>
+    <!-- Rounded switch: op zoek naar een buddy  -->
+
+<p>Ik ben op zoek naar een buddy ☺ </p>
+<label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
+ <!-- Rounded switch: ik ben een buddy  -->
+ <p>Ik ben een buddy ☺ </p>
+<label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
 </body>
 </html>
