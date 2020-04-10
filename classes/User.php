67b8fe7985e9b4028d11e9dc52f541e5012f7ff5
madoska <medina-dadurgova@hotmail.com>
2020-04-10 16:39:33 +0200

    fixed code formatting to be more uniform

--- classes/User.php
+++ classes/User.php
@@ -1,8 +1,8 @@
 <?php
-include_once (__DIR__ . "/Db.php");
-include_once (__DIR__ . "/Hobby.php");
-    class User 
-    {
+include_once(__DIR__ . "/Db.php");
+include_once(__DIR__ . "/Hobby.php");
+class User
+{
     private $email;
     private $password;
     private $firstName;
@@ -10,10 +10,10 @@ include_once (__DIR__ . "/Hobby.php");
     private $buddy;
     private $userBuddy;
 
-    
+
     /**
      * Get the value of email
-     */ 
+     */
     public function getEmail()
     {
         return $this->email;
@@ -23,7 +23,7 @@ include_once (__DIR__ . "/Hobby.php");
      * Set the value of email
      *
      * @return  self
-     */ 
+     */
     public function setEmail($email)
     {
         $this->email = $email;
@@ -33,7 +33,7 @@ include_once (__DIR__ . "/Hobby.php");
 
     /**
      * Get the value of password
-     */ 
+     */
     public function getPassword()
     {
         return $this->password;
@@ -43,28 +43,28 @@ include_once (__DIR__ . "/Hobby.php");
      * Set the value of password
      *
      * @return  self
-     */ 
+     */
     public function setPassword($password)
     {
         $this->password = $password;
-        
+
 
         //return $this;
     }
 
-     /**
+    /**
      * Get the value of password
-     */ 
-     public function getFirstname()
-     {
-         return $this->firstName;
-     }
- 
+     */
+    public function getFirstname()
+    {
+        return $this->firstName;
+    }
+
 
     /**
-    *Set the value of firstname
-    *  @return  self
-     */ 
+     *Set the value of firstname
+     *  @return  self
+     */
     public function setFirstname($firstName)
     {
         $this->firstName = $firstName;
@@ -74,17 +74,17 @@ include_once (__DIR__ . "/Hobby.php");
 
     /**
      * Get the value of lastName
-     */ 
-     public function getLastName()
-     {
-         return $this->lastName;
-     }
- 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
 
     /**
-    *Set the value of lastName
-    *  @return  self
-     */ 
+     *Set the value of lastName
+     *  @return  self
+     */
     public function setLastname($lastName)
     {
         $this->lastName = $lastName;
@@ -92,45 +92,45 @@ include_once (__DIR__ . "/Hobby.php");
         return $this;
     }
 
-     //get en set buddyCheckbox
-     public function getBuddy(){
+    //get en set buddyCheckbox
+    public function getBuddy()
+    {
         return $this->buddy;
     }
 
-    public function setBuddy($buddy){
+    public function setBuddy($buddy)
+    {
         $this->buddy = $buddy;
-
-        
     }
 
-    public function getUserBuddy(){
+    public function getUserBuddy()
+    {
         return $this->userBuddy;
     }
 
-    public function setUserBuddy($userBuddy){
+    public function setUserBuddy($userBuddy)
+    {
         $this->userBuddy = $userBuddy;
-
-        
     }
-    
 
 
-    
+
+
     public function canILogin()
     {
-    $conn = Db::getConnection();
+        $conn = Db::getConnection();
 
-    $statement = $conn->prepare('select * from user where email = :email');
-    $email = $this->getEmail();
-    $password = $this->getPassword();
-    $statement->bindParam(':email', $email);
-    $statement->execute();
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
-    if (password_verify($password, $result['password'])) {
-        return true;
-    } else {
-        return false;
-    }
+        $statement = $conn->prepare('select * from user where email = :email');
+        $email = $this->getEmail();
+        $password = $this->getPassword();
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($password, $result['password'])) {
+            return true;
+        } else {
+            return false;
+        }
     }
     // vraag userId op via de persoon zijn email
     public function getUserId()
@@ -145,60 +145,61 @@ include_once (__DIR__ . "/Hobby.php");
         return $result;
     }
 
-     public static function userSearch($search, $email)
+    public static function userSearch($search, $email)
     {
-    $conn = Db::getConnection();
+        $conn = Db::getConnection();
 
-    $statement = $conn->prepare("select * from user where firstname or lastname like :search and email != :email");
-    $statement->bindValue(':email', $email);
-    $statement->bindValue(':search', '%' . $search . '%');
-    $statement->execute();
-    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+        $statement = $conn->prepare("select * from user where firstname or lastname like :search and email != :email");
+        $statement->bindValue(':email', $email);
+        $statement->bindValue(':search', '%' . $search . '%');
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
     }
 
 
-    public function registerNewUser(){
-        $conn = Db::getConnection();    
-            
-            //Hash the password  
-        
+    public function registerNewUser()
+    {
+        $conn = Db::getConnection();
+
+        //Hash the password  
+
         $this->password = password_hash($this->password, PASSWORD_BCRYPT, ["cost" => 12]);
-        
-            //Registratie in database
-        $statement= $conn->prepare("INSERT INTO user (firstname, lastname, email, password, buddy, avatar) values (:firstname, :lastname, :email, :password, :buddy, :avatar)");
-        $statement->bindValue(":firstname",$this->firstName);
-        $statement->bindValue(":lastname",$this->lastName);
-        $statement->bindValue(":email",$this->email);
-        $statement->bindValue(":password",$this->password);
+
+        //Registratie in database
+        $statement = $conn->prepare("INSERT INTO user (firstname, lastname, email, password, buddy, avatar) values (:firstname, :lastname, :email, :password, :buddy, :avatar)");
+        $statement->bindValue(":firstname", $this->firstName);
+        $statement->bindValue(":lastname", $this->lastName);
+        $statement->bindValue(":email", $this->email);
+        $statement->bindValue(":password", $this->password);
         $statement->bindValue(":buddy", $this->buddy);
         $statement->bindValue(":avatar", "default.png");
 
-        $result=$statement->execute();
+        $result = $statement->execute();
 
         return $result;
-  
     }
-   
+
     //om al de buddies uit de database te halen
-    static function getAllBuddies($currentUser){
+    static function getAllBuddies($currentUser)
+    {
         $conn = Db::getConnection();
 
         $statement = $conn->prepare("SELECT * FROM buddies where buddy1ID = :currentUser OR buddy2ID = :currentUser");
         //$statement = $conn->prepare("SELECT u.firstname, u.lastname* FROM buddies as b, user u 
         //WHERE (u.userID = b.buddy1ID OR u.userID = b.buddy2ID) AND (buddy1ID = :currentUser OR buddy2ID = :currentUser)");
-        $statement->bindValue(":currentUser",$currentUser);
-        if($statement->execute()){
+        $statement->bindValue(":currentUser", $currentUser);
+        if ($statement->execute()) {
             return $statement->fetchAll(PDO::FETCH_ASSOC);
         }
-
     }
     //database statement om aan te passen of je buddy bent of een buddy zoekt
-   public function updateUserBuddy(){
+    public function updateUserBuddy()
+    {
 
-    $conn = Db::getConnection();
-    $statement = $conn->prepare("UPDATE user SET buddy='$this->buddy' WHERE userID = '$this->userBuddy'");
-    $result=$statement->execute();
-    return $result;
-   }    
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("UPDATE user SET buddy='$this->buddy' WHERE userID = '$this->userBuddy'");
+        $result = $statement->execute();
+        return $result;
+    }
 }
