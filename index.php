7f25f419aa9c4899f2bb15950346198553d505db
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-04-24 17:24:01 +0200

    link to css styling

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ $posts = $fetchPosts->fetchPosts();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Amigos</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
