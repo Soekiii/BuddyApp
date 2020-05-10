a2d4685ca5b38302a8dfdb41b6ba479f98023972
Soekiii <61419115+Soekiii@users.noreply.github.com>
2020-05-10 12:05:09 +0200

    functie aanemaakt om een profiel te creeëren met avatar en bio

--- users.php
+++ users.php
@@ -12,8 +12,8 @@ if(isset($_GET['id'])){
     $data = $buddyData->buddyData($buddyID);
 
     $user = new User();
-    $userData = $User->getUserById($_GET['id']);
-    var_dump($userData);
+    $result = $user->getUserById($_GET['id']);
+    //var_dump($userData);
 }
 ?>
 
@@ -25,14 +25,21 @@ if(isset($_GET['id'])){
     <title>Document</title>
 </head>
 <body>
-    <h3><?php echo $data['firstname'] . " " . $data['lastname']; ?></h3>
+
+<img src="avatars/<?php echo $result["avatar"]?>"> <h3><?php echo $data['firstname'] . " " . $data['lastname']; ?></h3>
+<p><?php echo $result['bio'] ?></p>
+
 
     <?php foreach($buddyOthers as $buddy) : ?>
     <?php if($buddy['status'] == 1){ ?>
         <div class="buddy">
             <a href="users.php?id=<?php echo $buddy['userID'] ?>"><?php echo $buddy['firstname'] . " " . $buddy['lastname'] ?></a> <?php echo "'s buddy"; ?>
+           
+           
+            
         </div>
     <?php } ?>
     <?php endforeach; ?>
 </body>
 </html>
+
