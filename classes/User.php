f3a3804a03c8c9372e34024edef1cd1bd0ed7f24
madoska <medina-dadurgova@hotmail.com>
2020-05-10 19:19:46 +0200

    fixed activation link

--- classes/User.php
+++ classes/User.php
@@ -248,7 +248,7 @@ class User
         return $result;
     }
     public function activatieLink($id,$token){
-        $link = "<a href='http://imdamigos.site/BuddyApp/activatie.php?token=$token&userID=$id'>" . 'Activeer Account' . '</a>';
+        $link = "<a href='http://www.imdamigos.site/activatie.php?token=$token&userID=$id'>" . 'Activeer Account' . '</a>';
         return $link;
     }
     
